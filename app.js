bc1caf7c97060ba2ad7beecc204913eba9b89f66
unknown <lanoguera@poligran.edu.co>
2020-09-07 20:49:55 -0500

    commit

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ appServer.listen(3000, () => {
 
 appServer.use(express.json());
 
-let list_juegos = [];
+let listJuegos = [];
 
-appServer.post("/crearjuego", (req, res) => {
+appServer.post("/crearJuego", (req, res) => {
   
   let juego = {};
   juego.id = req.body.id;
@@ -28,42 +28,42 @@ appServer.post("/crearjuego", (req, res) => {
   juego.lanzamiento = req.body.lanzamiento;
   juego.descripcion = req.body.descripcion;
   juego.edadmin = req.body.edadmin;
-  list_juegos.push(juego);
+  listJuegos.push(juego);
 
   res.send(juego);
   console.log("JUEGO CREADO");
-  console.log(list_juegos);
+  console.log(listJuegos);
 });
 
-appServer.delete("/eliminarjuego/:idjuego", (req, res) => {
-  list_juegos = list_juegos.filter(juego => juego.id != req.params.idjuego);
+appServer.delete("/eliminarJuego/:idJuego", (req, res) => {
+  listJuegos = listJuegos.filter(juego => juego.id != req.params.idJuego);
   console.log("JUEGO ELIMINADO");
-  console.log(list_juegos);
-  res.send(list_juegos);
+  console.log(listJuegos);
+  res.send(listJuegos);
 });
 
-appServer.get("/mostrarjuegos", (req, res) => {
-  res.send(list_juegos);
+appServer.get("/mostrarJuegos", (req, res) => {
+  res.send(listJuegos);
   console.log("LISTA DE JUEGOS:");
-  console.log(list_juegos);
+  console.log(listJuegos);
 });
 
-appServer.get("/mostrarjuegoid/:idjuego", (req, res) => {
-  let juego = list_juegos.find(element => element.id == req.params.idjuego);
+appServer.get("/mostrarJuegoid/:idjuego", (req, res) => {
+  let juego = listJuegos.find(element => element.id == req.params.idjuego);
   console.log("JUEGO CON ID: " + req.params.idjuego);
   console.log(juego);
   res.send(juego);
 });
 
-appServer.get("/mostrarjuegonom/:titulo", (req, res) => {
-  let juego = list_juegos.find(element => element.titulo == req.params.titulo);
+appServer.get("/mostrarJuegoNom/:titulo", (req, res) => {
+  let juego = listJuegos.find(element => element.titulo == req.params.titulo);
   console.log("JUEGO CON NOMBRE: " + req.params.titulo);
   console.log(juego);
   res.send(juego);
 });
 
-appServer.get("/mostrarlanzamiento/:lanzamiento", (req, res) => {
-  let juego = list_juegos.filter(element => element.lanzamiento < req.params.lanzamiento);
+appServer.get("/mostrarLanzamiento/:lanzamiento", (req, res) => {
+  let juego = listJuegos.filter(element => element.lanzamiento < req.params.lanzamiento);
   console.log("JUEGOS CON AÑO MENOR A: " + req.params.lanzamiento);
   console.log(juego);
   res.send(juego);
